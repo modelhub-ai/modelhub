9a46ce10ed3006b924bf6b37a08361b6347b0f88
Michael Schwier <mschwier@bwh.harvard.edu>
2018-05-01 15:19:17 -0400

    Simplified run.py

--- squeezenet/usr_src/run.py
+++ squeezenet/usr_src/run.py
@@ -1,16 +1,4 @@
-import webservice
-import netron
-import sys
-import time
-from multiprocessing import Process
-
-
-def startNetron():
-    netron.serve_file("/usr_src/model/model.onnx", port=81, host="0.0.0.0")
-
+import main
 
 if __name__ == "__main__":
-    netronProcess = Process(target=startNetron)
-    netronProcess.start()
-    webservice.start()
-    netronProcess.terminate()
+    main.start()
