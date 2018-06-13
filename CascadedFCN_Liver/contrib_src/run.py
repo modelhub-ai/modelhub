10cb417b6df57a0940379dd42a8445de41139ee8
Michael Schwier <mschwier@bwh.harvard.edu>
2018-06-13 19:41:28 -0400

    Adapted to using the new modelhubapi package

--- CascadedFCN_Liver/contrib_src/run.py
+++ CascadedFCN_Liver/contrib_src/run.py
@@ -1,6 +1,8 @@
-import main
+import os
+from modelhubapi import webservice
 from inference import Model
 
 if __name__ == "__main__":
     model = Model()
-    main.start(model)
+    contrib_src_dir = os.path.dirname(os.path.realpath(__file__))
+    webservice.start(model, contrib_src_dir)
