79c34e86151195913ab5da41790837b8c789fbea
Michael Schwier <mschwier@bwh.harvard.edu>
2018-07-03 16:08:05 -0400

    moved to standalone repo

--- models/squeezenet/contrib_src/run.py
+++
@@ -1,8 +0,0 @@
-import os
-from modelhubapi import webservice
-from inference import Model
-
-if __name__ == "__main__":
-    model = Model()
-    contrib_src_dir = os.path.dirname(os.path.realpath(__file__))
-    webservice.start(model, contrib_src_dir)
