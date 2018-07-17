c7e806f50ff635428b6d6d3ad0c888ad89339d6a
Michael Schwier <mschwier@bwh.harvard.edu>
2018-07-17 16:12:23 -0400

    Template moved to its own repository

--- template/contrib_src/postprocessing.py
+++
@@ -1,9 +0,0 @@
-from modelhublib.postprocessor import PostprocessorBase
-import numpy as np
-import json
-
-class Postprocessor(PostprocessorBase):
-
-    def computeOutput(self, inferenceResults):
-        # TODO: implement postprocessing of inference results
-        return result
